547b3fae767e692663c3485aff09967f96643e97
Aryman Sharma <aryman.sharma125@gmail.com>
2024-01-23 20:16:41 +0530

    yhhh

---
+++ app.js
@@ -0,0 +1,42 @@
+const countvalue = document.querySelector('#counter');
+
+function increment()
+{
+// get value from ui since it is in string format we can use parseInt to convert it into int
+ let value = parseInt(countvalue.innerText);
+//   increment the value
+ value=value+1;
+ // vapis se value set kr di
+ countvalue.innerText=value;
+}
+const decrement = () =>
+{
+    let value = parseInt(countvalue.innerText);
+    //   increment the value
+     value=value-1;
+     // vapis se value set kr di
+     countvalue.innerText=value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
